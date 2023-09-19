da34195aa48676f2d91d48cdeb0180a28a50366a
Oong Jie Xiang <oongjiexiang@yahoo.com>
2023-09-19 17:42:55 +0800

    GitHub workflow

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,8 @@ server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/");
 });
 
-const MONGO_URL =
-  "mongodb+srv://clee080:passworD@cluster0.x62uono.mongodb.net/?retryWrites=true&w=majority";
-const MONGO_URL2 = "mongodb://localhost:27017/";
+const MONGO_URL = process.env.DBHOST;
+// const MONGO_URL2 = "mongodb://localhost:27017/";
 
 // mongoose.set("strictQuery", false);
 // mongoose.Promise = Promise;
